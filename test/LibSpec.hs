40cb48fc776cc6b09ce240e26113f9c6cb3ed4b9
ocramz <zocca.marco@gmail.com>
2018-07-23 14:44:47 +0200

    add test

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -3,7 +3,7 @@ module Main where
 import Test.Hspec
 -- import Test.Hspec.QuickCheck
 
-import Numeric.Classification.DecisionTrees 
+import Numeric.Classification.DecisionTrees
 
 
 main :: IO ()
@@ -12,8 +12,10 @@ main = hspec spec
 spec :: Spec
 spec =
   describe "Numeric.Classification.DecisionTrees" $ do
-    it "The entropy of a uniform distribution is == 1" $ 
-      entropy ds0 `shouldBe` Just 1.0
+    it "The (regularized) entropy of a uniform distribution is == 1" $ 
+      entropyR dsUniform `shouldBe` 1.0
+    it "The (regularized) entropy of a degenerate distribution is := 0" $ 
+      entropyR dsEmpty `shouldBe` 0.0      
   -- describe "Lib" $ do
   --   it "works" $ do
   --     True `shouldBe` True
@@ -21,5 +23,7 @@ spec =
     --   ourAdd x y `shouldBe` ourAdd y x
 
 
-ds0 :: Dataset Int [Double]
-ds0 = fromList [(1, [2,3,4,5]), (2, [1,2,3,5]) ]
+dsUniform, dsEmpty :: Dataset Int [Double]
+dsUniform = fromList [(1, [2,3,4,5]), (2, [1,2,3,5]) ]
+
+dsEmpty = fromList []
