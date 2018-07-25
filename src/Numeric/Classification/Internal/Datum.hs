896f236dc2e1ac52c550de28ba6cd8d0034df59d
ocramz <zocca.marco@gmail.com>
2018-07-25 12:42:13 +0200

    upd Datum

--- src/Numeric/Classification/Internal/Datum.hs
+++ src/Numeric/Classification/Internal/Datum.hs
@@ -1,47 +1,50 @@
 {-# language TypeFamilies #-}
-module Numeric.Classification.Internal.Datum where
+module Numeric.Classification.Internal.Datum (Datum(..), (!?), splitAttrP) where
 
 import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
 import qualified Data.Vector.Unboxed as VU
 
 class Datum d where
+  {-# minimal lookupAttribute #-}
   type DKey d :: *
   type DData d :: *
+  type Dkv d :: *
   lookupAttribute :: d -> DKey d -> Maybe (DData d)
+  fromList :: [Dkv d] -> d
 
 (!?) :: Datum d => d -> DKey d -> Maybe (DData d)
 (!?) = lookupAttribute  
 
+-- | A 'DenseD' datum is internally an unboxed vector
 newtype DenseD a = DD { unDD :: VU.Vector a } deriving (Eq, Show)
-
-fromListDD :: VU.Unbox a => [a] -> DenseD a
-fromListDD ll = DD $ VU.fromList ll
 
 instance VU.Unbox a => Datum (DenseD a) where
   type DKey (DenseD a) = Int
   type DData (DenseD a) = a
+  type Dkv (DenseD a) = a
   lookupAttribute (DD v) i = v VU.!? i
+  fromList = DD . VU.fromList
 
+-- | A 'SparseD' datum is internally a 'Map'
 newtype SparseD k a = SD { unSD :: M.Map k a } deriving (Eq, Show)
-
-fromListSD :: Ord k => [(k, a)] -> SparseD k a
-fromListSD ll = SD $ M.fromList ll
 
 instance Ord k => Datum (SparseD k a) where
   type DKey (SparseD k a) = k 
   type DData (SparseD k a) = a
+  type Dkv (SparseD k a) = (k, a)
   lookupAttribute (SD mm) i = M.lookup i mm
+  fromList = SD . M.fromList
 
+-- | A 'SparseDI' datum is internally an 'IntMap'
 newtype SparseDI a = SDI { unSDI :: IM.IntMap a } deriving (Eq, Show)
-
-fromListSDI :: [(IM.Key, a)] -> SparseDI a
-fromListSDI ll = SDI $ IM.fromList ll
 
 instance Datum (SparseDI a) where
   type DKey (SparseDI a) = IM.Key
   type DData (SparseDI a) = a
+  type Dkv (SparseDI a) = (IM.Key, a)
   lookupAttribute (SDI mm) i = IM.lookup i mm
+  fromList = SDI . IM.fromList
 
 
 
