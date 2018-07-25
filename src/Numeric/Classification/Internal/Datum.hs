2f7322e03a822e68166225624a37b5b152f78fb5
ocramz <zocca.marco@gmail.com>
2018-07-25 17:04:29 +0200

    fix Datum

--- src/Numeric/Classification/Internal/Datum.hs
+++ src/Numeric/Classification/Internal/Datum.hs
@@ -1,8 +1,9 @@
-{-# language TypeFamilies, MultiParamTypeClasses, InstanceSigs, RankNTypes #-}
+{-# language TypeFamilies, MultiParamTypeClasses, DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 module Numeric.Classification.Internal.Datum
  --   (Datum(..), (!?), splitAttrP)
  where
 
+import qualified Data.Foldable as F
 import qualified Data.IntMap.Strict as IM
 import qualified Data.Map.Strict as M
 -- import qualified Data.Vector.Unboxed as VU
@@ -13,9 +14,9 @@ import qualified Data.Map.Strict as M
 splitAttrP :: Datum d => (a -> Bool) -> Key d -> (d a -> Bool)
 splitAttrP p k dat = maybe False p (dat !? k)
 
-class Datum (d :: * -> *) where
+class Foldable d => Datum (d :: * -> *) where
 --   {-# minimal lookupAttribute, fromList #-}
-  type Key d :: *
+  type Key d :: *  -- ^ Feature
   type V d a :: *
   lookupAttribute :: d a -> Key d -> Maybe a
   fromList :: [V d a] -> d a
@@ -24,10 +25,13 @@ class Datum (d :: * -> *) where
 (!?) :: Datum d => d a -> Key d -> Maybe a
 (!?) = lookupAttribute  
 
+maximumD, minimumD :: (Datum d, Ord a) => d a -> a
+maximumD = maximum . F.toList
+minimumD = minimum . F.toList
 
 
 -- | A 'SparseD' datum is internally a 'Map'
-newtype SparseD k a = SD { unSD :: M.Map k a } deriving (Eq, Show)
+newtype SparseD k a = SD { unSD :: M.Map k a } deriving (Eq, Show, Functor, Foldable, Traversable)
 
 instance Ord k => Datum (SparseD k) where
   type Key (SparseD k) = k 
@@ -37,7 +41,7 @@ instance Ord k => Datum (SparseD k) where
   imap f (SD mm) = SD $ M.mapWithKey f mm
 
 -- | A 'SparseDI' datum is internally an 'IntMap'
-newtype SparseDI a = SDI { unSDI :: IM.IntMap a } deriving (Eq, Show)
+newtype SparseDI a = SDI { unSDI :: IM.IntMap a } deriving (Eq, Show, Functor, Foldable, Traversable)
 
 instance Datum SparseDI where
   type Key SparseDI = IM.Key
