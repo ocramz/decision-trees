38f99ce0caa840d196594882b1a47a8f0961e465
ocramz <zocca.marco@gmail.com>
2018-07-28 12:13:49 +0200

    embed Iris dataset in Double^5

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,22 @@
+{-# language DeriveGeneric #-}
 module Main where
 
 import Numeric.Classification
 
-import Numeric.Datasets.Iris
+import Numeric.Datasets.Iris (Iris(..), IrisClass(..), iris)
+
+import GHC.Generics
+
+import Control.Monad.Catch (MonadThrow(..))
+import qualified Data.Vector as V
+
+irisV :: MonadThrow m => Iris -> m (V Double)
+irisV (Iris sl sw pl pw iclass) = mkV 5 $ V.fromList [sl, sw, pl, pw, iclassd] where
+  iclassd = fromIntegral (fromEnum iclass)
 
 
-main = print $ head iris
+main = do --print $ head iris
+  iv <- traverse irisV iris -- irisV $ head iris
+  print iv
+
+
--- src/Numeric/Classification.hs
+++ src/Numeric/Classification.hs
@@ -1,3 +1,13 @@
-module Numeric.Classification where
+module Numeric.Classification (
+  -- * Tree
+  Tree, TOptions(..), TNData(..), growTree,
+  -- * Dataset
+  Dataset(..), uniques,
+  -- * Data point
+  XV.V(..), XV.mkV
+  ) where
 
-
+import Data.Dataset
+import Numeric.Classification.DecisionTrees
+import qualified Numeric.Classification.Internal.Datum.Vector as XV
+import Numeric.Classification.Exceptions (DataException(..))
